e4e56364e8efb91b6e3f7acb2b1ef6c281d1480c
knickheal75 <blitzkrieg0705@gmail.com>
2019-03-24 14:53:36 +0530

    invoice in react

---
+++ invoiceapp/src/components/Info.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Info extends React.Component{
+    render(){
+    return(
+    <center>
+    <h1>INVOICE</h1> 
+    </center>
+    )
+}
+}
+export default Info;
---
+++ invoiceapp/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Table from "./components/Table";
+import Info from "./components/Info";
+
+
+ReactDOM.render(<Table />, document.getElementById('root'));
+
+
+ReactDOM.render(<Info />, document.getElementById('info'));
