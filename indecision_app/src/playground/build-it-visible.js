c3e38c68b91e173e94f02c5b7210329a0a81273a
knickheal75 <blitzkrieg0705@gmail.com>
2019-10-30 19:27:36 +0530

    webpack additions

--- indecision_app/src/playground/build-it-visible.js
+++ indecision_app/src/playground/build-it-visible.js
@@ -1,22 +1,31 @@
-let count = false;
-const showDetail = () => {
-  count = !count;
-  render();
-};
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      count: false
+    };
+  }
+  handleToggleVisibility() {
+    this.setState(prevState => {
+      return {
+        visibility: !prevState.visibility
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility ? "Hide Details" : "Show Details"}
+        </button>
+        <p>
+          {this.state.visibility ? "Hello,now you can see the details" : ""}
+        </p>
+      </div>
+    );
+  }
+}
 
-const appRoot = document.getElementById("app");
-
-const render = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={showDetail}>
-        {count ? "Hide Details" : "Show Details"}
-      </button>
-      <p>{count ? "Hello,now you can see the details" : ""}</p>
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
-};
-
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById("app"));
